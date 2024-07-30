de9f7e844ff23dd0edfe4f1306651584c51befdf
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2024-07-30 09:58:30 -0300

    Base

---
+++ app/src/main/java/com/example/app_ods/ods12.java
@@ -0,0 +1,14 @@
+package com.example.app_ods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ods12 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ods12);
+    }
+}
